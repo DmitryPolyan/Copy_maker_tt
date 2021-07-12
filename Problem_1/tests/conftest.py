aab94cf4e7fe9c98f72fd1a4f9ab6e2da680beea
Dmitry Polyanskiy <dmitrystomper@gmail.com>
2021-07-12 22:42:30 +0300

    work continues

--- Problem_1/tests/conftest.py
+++ Problem_1/tests/conftest.py
@@ -1,20 +1,20 @@
-import pytest
-import platform
-import os
-
-
-@pytest.fixture(autouse=True, scope='session')
-def preparing_and_cleaning():
-    if platform.system() == "Linux":
-        if os.path.isfile('/etc/boot.log'):
-            os.remove("/etc/boot.log")
-    else:
-        if os.path.isfile(r'C:\Program files\kernel32.dll'):
-            os.remove(r"C:\Program files\kernel32.dll")
-    yield
-    if platform.system() == "Linux":
-        if os.path.isfile('/etc/boot.log'):
-            os.remove("/etc/boot.log")
-    else:
-        if os.path.isfile(r'C:\Program files\kernel32.dll'):
-            os.remove(r"C:\Program files\kernel32.dll")
+# import pytest
+# import platform
+# import os
+#
+#
+# @pytest.fixture
+# def preparing_and_cleaning():
+#     if platform.system() == "Linux":
+#         if os.path.isfile('/etc/boot.log'):
+#             os.remove("/etc/boot.log")
+#     else:
+#         if os.path.isfile(r'C:\Program files\kernel32.dll'):
+#             os.remove(r"C:\Program files\kernel32.dll")
+#     yield
+#     if platform.system() == "Linux":
+#         if os.path.isfile('/etc/boot.log'):
+#             os.remove("/etc/boot.log")
+#     else:
+#         if os.path.isfile(r'C:\Program files\kernel32.dll'):
+#             os.remove(r"C:\Program files\kernel32.dll")
--- Problem_1/tests/test_problem_1.py
+++ Problem_1/tests/test_problem_1.py
@@ -1,2 +1,6 @@
-def test_copying ():
-    ...
+import problem_1.copy_maker as cm
+
+
+def test_parser_xml():
+    result = cm.parser_xml('tests/config.xml')
+    assert isinstance(result, dict)
